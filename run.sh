f61263e7ea5befa71898e484c38b64a37b32fe00
Delson Lima <dev@delsonlima.com.br>
2023-09-24 23:04:50 -0300

    Change log file location

--- run.sh
+++ run.sh
@@ -5,12 +5,15 @@ export LOG_LEVEL="trace"
 bashio::log.info "Configuring Matrix Synapse..."
 
 DATA_DIR="$(bashio::config 'data_dir')"
+LOG_DIR="${DATA_DIR}/logs"
 SERVER_NAME="$(bashio::config 'server_name')"
 REPORT_STATS="$(bashio::config 'report_stats')"
 SERVER_CONF="${DATA_DIR}/homeserver.yaml"
+LOG_CONF="${DATA_DIR}/${SERVER_NAME}.log.config"
 ENABLE_STATS=$([ "$REPORT_STATS" = true ] && echo "yes" || echo "no")
 
 mkdir -p "${DATA_DIR}"
+mkdir -p "${LOG_DIR}"
 
 synapse_homeserver \
     --data-directory "${DATA_DIR}" \
@@ -20,8 +23,16 @@ synapse_homeserver \
     --report-stats="${ENABLE_STATS}"
 
 SERVER_CONF_CONTENT=$(cat "${SERVER_CONF}")
+LOG_CONF_CONTENT=$(cat "${LOG_CONF}")
 
-yq '.listeners[0].bind_addresses = ["0.0.0.0"]' <<< "$SERVER_CONF_CONTENT" > "${SERVER_CONF}"
+yq '.listeners[0].bind_addresses = ["0.0.0.0"]' \
+  <<< "$SERVER_CONF_CONTENT" \
+  > "${SERVER_CONF}"
+
+LOG_FILE="${LOG_DIR}/homeserver.log" \
+  yq '.handlers.file.filename = env(LOG_FILE)' \
+    <<< "${LOG_CONF_CONTENT}" \
+    > "${LOG_CONF}"
 
 bashio::log.info "Starting Matrix Synapse..."
 
